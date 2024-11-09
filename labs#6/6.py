f25a4236d88bd54548c39df5be140617fadb8b63
VadimPirkovskiy <vadimpirkovskij8@gmail.com>
2024-11-09 13:03:50 +0300

    Update 6.py

--- labs#6/6.py
+++ labs#6/6.py
@@ -7,6 +7,8 @@ for i in range(0,k):
     if n[i]=='a':
         c+=1
 u = n.split('а')
-u = n.split('a')
-u = ''.join(u)
-print(c, u)
+u = str(u)
+u = u.split('a')
+u = ''.join(u)a
+e = str(u)
+print(c,e)
